debf7cb38736a278cef29be2b70ce7a26e25372c
Yun Jae <yunjae@Yuns-Air.lan>
2023-10-26 17:34:47 -0600

    Create database to mock api

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,10 +8,14 @@ const Home = () => {
     { title: "Web dev top tips", body: "lorem ipsum", author: "mario", id: 3 },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {}, []);
 
   return (
     <div className="home">
