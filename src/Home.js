8b99cf2c8029ee6e129edaf43f3944297b67f79a
Yun Jae <yunjae@Yuns-Air.lan>
2023-10-25 19:36:53 -0600

    Test home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const Home = () => {
+  // let name = "Mario";
+  const [name, setName] = useState("Mario");
+  const handleClick = () => {
+    setName("Luigi");
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <p>{name}</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
