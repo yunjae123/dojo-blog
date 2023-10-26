a30781063404634bef07e7a0d36473639a3d0db0
Yun Jae <yunjae@Yuns-Air.lan>
2023-10-26 13:49:15 -0600

    Add handleDelete function that deletes a blog upon clicking

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,14 @@
-const BlogList = (props) => {
-  const blogs = props.blogs;
+const BlogList = ({ blogs, title, handleDelete }) => {
+  // const blogs = props.blogs;
+  // const title = props.title;
   return (
     <div className="blog-list">
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
