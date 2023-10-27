78504b7dc53e5d5f3159de755a5d9d02a32b932c
Yun Jae <yunjae@Yuns-Air.lan>
2023-10-27 14:14:17 -0600

    Create custom hook useFetch and separate file for reuseability

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,18 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 // npx json-server --watch data/db.json --port 8000
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (res.ok) {
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div> Loading... </div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
