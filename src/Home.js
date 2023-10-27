381dac5352c398ab17ece560ae04e2334a06e5e2
Yun Jae <yunjae@Yuns-Air.lan>
2023-10-26 18:58:54 -0600

    Create useEffect hook function to setBlogs from a json server and change the status of isPending

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,24 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum", author: "mario", id: 1 },
-    { title: "Welcome party", body: "lorem ipsum", author: "yoshi", id: 2 },
-    { title: "Web dev top tips", body: "lorem ipsum", author: "mario", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-  const [name, setName] = useState("mario");
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {isPending && <div> Loading... </div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
