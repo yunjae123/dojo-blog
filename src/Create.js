5dcb5904332805287cc54a80da03e4ce0c916786
Yun Jae <yunjae0500@yahoo.com>
2023-11-02 19:51:01 -0600

    Add functionality to create.js where after posting a blog, the page auto-redirects to home

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
@@ -18,6 +21,7 @@ const Create = () => {
     }).then(() => {
       console.log("New blog added");
       setIsPending(false);
+      history.push("/");
     });
   };
   return (
